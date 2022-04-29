194a3e3aadcfae8bd5cf3139a9566e801ec2b363
titzer <titzer@users.noreply.github.com>
2022-04-29 15:19:09 +0000

    deploy: 9687d15d443133a0a2286f5727717aab5f06c3b2

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 + function references (Draft 2022-03-22)',
+    VERSION: '1.1 + function references (Draft 2022-04-29)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
